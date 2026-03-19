bedf4b3637bace417f8900121575dc6be9d4109b
Martin F <martinf@MacBook-Pro-de-hipto--Martin-F.local>
2026-03-19 10:40:49 +0100

    fix: image path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "oferta-seguro",
+  build: {
+    outDir: "oferta-seguro",
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+      },
+    },
+  },
+  server: {
+    host: "0.0.0.0",
+  },
+});
